91607e2e04e75fcc096eeac82dd1d785557fcb39
DESKTOP-5UEQ9PJ\GOLD <goldtime0820@gmail.com>
2019-01-12 22:54:33 -0500

    list

---
+++ components/List/List.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+const List = (props) => {
+  const ComponentToRender = props.component;
+  let content = (<div></div>);
+
+  // If we have items, render them
+  if (props.items) {
+    content = props.items.map((item) => (
+      <div key={`item-${item}`} >
+        {item}
+      </div>
+    ));
+  } else {
+    // Otherwise render a single component
+    content = (<ComponentToRender />);
+  }
+
+  return (
+    <div className="list-wrapper">
+      <ul>
+        {content}
+      </ul>
+    </div>
+  );
+};
+
+List.propTypes = {
+  component: PropTypes.any,
+  items: PropTypes.any,
+};
+
+export default List;
