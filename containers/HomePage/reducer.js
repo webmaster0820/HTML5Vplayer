331c3cc505deaf0623d6a589ab3703904fa8f583
DESKTOP-5UEQ9PJ\GOLD <goldtime0820@gmail.com>
2019-01-19 22:59:22 -0500

    update

---
+++ containers/HomePage/reducer.js
@@ -0,0 +1,31 @@
+/*
+ * HomeReducer
+ *
+ * The reducer takes care of our data. Using actions, we can change our
+ * application state.
+ * To add a new action, add it to the switch statement in the reducer function
+ *
+ * Example:
+ * case YOUR_ACTION_CONSTANT:
+ *   return state.set('yourStateVariable', true);
+ */
+import { fromJS } from 'immutable';
+
+import { CHANGE_SEGINFO } from './constants';
+
+// The initial state of the App
+const initialState = fromJS({
+  segInfo :{}
+});
+
+function homeReducer(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_SEGINFO:
+      // Delete prefixed '@' from the github username
+      return state.set('segInfo', action.info);
+    default:
+      return state;
+  }
+}
+
+export default homeReducer;
