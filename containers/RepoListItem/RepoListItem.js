8e6f74452ece39b7107ce58b70d6374d73796064
DESKTOP-5UEQ9PJ\GOLD <goldtime0820@gmail.com>
2019-01-20 23:00:20 -0500

    update

---
+++ containers/RepoListItem/RepoListItem.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import ListItem from 'components/ListItem';
+import { IssueIcon } from 'components/Icons';
+import './style.scss';
+
+export default class SegsListItem extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    let nameprefix = '';
+
+    const content = (
+      <div className="segs-list-item">
+      </div>
+    );
+
+    // Render the content into a list item
+    return (
+      <div/>
+    );
+  }
+}
+
---
+++ containers/RepoListItem/index.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import SegsListItem from './SegsListItem';
+
+export default connect(
+  createStructuredSelector({
+  })
+)(SegListItem);
